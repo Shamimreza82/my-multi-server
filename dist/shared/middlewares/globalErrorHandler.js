fc45d62a9df2748ae8b8587200dc94ae7b933416
Shamimreza82 <shamimrezaone@gmail.com>
2025-05-07 20:58:59 +0600

    Remove node_modules from repository

--- dist/shared/middlewares/globalErrorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.globalErrorHandler = globalErrorHandler;
-function globalErrorHandler(err, req, res, next) {
-    console.error(err);
-    res.status(err.statusCode ?? 500)
-        .json({ message: err.message ?? 'Internal Server Error' });
-}
