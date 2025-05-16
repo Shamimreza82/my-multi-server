139f71192b3659bd77931b5c3d3d9eda6dfd6b4c
Shamimreza82 <shamimrezaone@gmail.com>
2025-05-16 06:26:45 +0600

    update scha

--- src/app.ts
+++ src/app.ts
@@ -90,7 +90,7 @@ io.on('connection', (socket) => {
                     you seeling service web developer.
                     your eamil: shamimrezaone@gmail.com 
                     your phone: +8801531297879
-                   
+                   --------
                     Whenever the user says “how are you,” reply with: “i am fine—thank you! How can I help you today?” line.
                     Whenever the user says “who are you” reply with: “I am shamim reza full stack web developer how can i help” line.
  
